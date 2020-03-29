1ea6bf3ff2915e9436fe207f74bf7326a14b5c33
shiva <shivachaitanyas3@gmail.com>
2020-03-29 01:58:15 -0400

    updates on sensor listener class

--- app/src/main/java/com/example/healthtracker/MainActivity.kt
+++ app/src/main/java/com/example/healthtracker/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity() {
 
             // start the background service
             if(idlenessStatus) {
-                imgBtnIdleness.setImageResource(R.drawable.pause)
+                imgBtnIdleness.setImageResource(R.drawable.stop)
                 txtViewIdleness.text = getString(R.string.STOP_IDLE_BUTTON)
                 Toast.makeText(this, "Idleness tracking activated!", Toast.LENGTH_SHORT).show()
                 val serviceIntent = Intent(this,IdlenessService::class.java)
@@ -72,7 +72,7 @@ class MainActivity : AppCompatActivity() {
 
             // start the background service
             if(screenTimeStatus) {
-                imgBtnScreenTime.setImageResource(R.drawable.pause)
+                imgBtnScreenTime.setImageResource(R.drawable.stop)
                 txtViewScreenTime.text = getString(R.string.STOP_SCREEN_BUTTON)
                 Toast.makeText(this, "Screen time tracking activated!", Toast.LENGTH_SHORT).show()
                 val serviceIntent = Intent(this,ScreenTimeService::class.java)
